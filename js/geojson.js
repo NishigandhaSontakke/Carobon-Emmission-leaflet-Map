a47cb379f292628d4d8a5c5db6104ca4ee165cef
Nishigandha Sontakke <sontakke@wisc.edu>
2019-03-05 21:38:14 -0600

    Module 5B assignment completed
    Module 5B assignment completed

--- js/geojson.js
+++ js/geojson.js
@@ -1,5 +1,8 @@
 /* Map of GeoJSON data from emission.geojson */
 
+// Global variable to hold layer
+var searchLayer;
+
 //function to instantiate the Leaflet map
 function createMap(){
     //create the map
@@ -42,72 +45,125 @@ function calcPropRadius(attValue) {
     return radius;
 };
 
-function createPropSymbols(data, map){
 
-    var attribute = "1990 Co2";
 
-    //create marker geojsonMarkerOptions
-    var geojsonMarkerOptions = {
-        radius: 8,
-        fillColor: "#ff7800",
-        color: "#000",
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("Co2") > -1){
+            attributes.push(attribute);
+        };
     };
 
-    //create a Leaflet GeoJSON layer and add it to the map
-    L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-             //For each feature, determine its value for the selected attribute
-             var attValue = Number(feature.properties[attribute]);
+    //check result
+    console.log(attributes);
 
-             //Give each feature's circle marker a radius based on its attribute value
-             geojsonMarkerOptions.radius = calcPropRadius(attValue);
- 
-            //create circle marker layer
-            var layer = L.circleMarker(latlng, geojsonMarkerOptions);
-
-            //build popup content string
-            var popupContent = "<p><b>State:</b> " + feature.properties.State + "</p><p><b>" + attribute + ":</b> " + feature.properties[attribute] + "</p><p>Values of Carbon are in Metric tons</p><p><b>Energy Source: </b>"+ feature.properties.Energy+ "</p><p><b>";
-
-            //bind the popup to the circle marker
-            layer.bindPopup(popupContent, {
-                offset: new L.Point(0,- geojsonMarkerOptions.radius) 
-            });
-
-            //event listeners to open popup on hover
-            layer.on({
-                mouseover: function(){
-                    this.openPopup();
-                },
-                mouseout: function(){
-                    this.closePopup();
-                }
-            });
-
-            //return the circle marker to the L.geoJson pointToLayer option
-            return layer;
-        }
-    }).addTo(map);
+    return attributes;
 };
+function createPropSymbols(data, map, attributes){
 
-
+    // var attribute = "1990 Co2";
+   searchLayer= L.geoJson(data, {
+     pointToLayer: function(feature, latlng){
+         return pointToLayer(feature, latlng, attributes);
+     }
+ }).addTo(map);
+ };
+ 
+ function pointToLayer(feature, latlng,attributes){
+ 
+     var attribute = attributes[0];
+     console.log(attribute);
+     //create marker geojsonMarkerOptions
+     var options = {
+         radius: 8,
+         fillColor: "#ff7800",
+         color: "#000",
+         weight: 1,
+         opacity: 1,
+         fillOpacity: 0.8
+     };
+     var attValue = Number(feature.properties[attribute]);
+ 
+     //Give each feature's circle marker a radius based on its attribute value
+     options.radius = calcPropRadius(attValue);
+ 
+     //create circle marker layer
+     var layer = L.circleMarker(latlng, options);
+ 
+     //build popup content string
+     var popupContent = "<p><b>State:</b> " + feature.properties.State + "</p><p><b>" + attribute + ":</b> " + feature.properties[attribute] + "</p><p>Values of Carbon are in Metric tons</p><p><b>Energy Source: </b>"+ feature.properties.Energy+ "</p><p><b>";
+ 
+     //bind the popup to the circle marker
+     layer.bindPopup(popupContent, {
+         offset: new L.Point(0,- options.radius) 
+     });
+ 
+     //event listeners to open popup on hover
+     layer.on({
+         mouseover: function(){
+             this.openPopup();
+         },
+         mouseout: function(){
+             this.closePopup();
+         }
+     });
+ 
+     //return the circle marker to the L.geoJson pointToLayer option
+     return layer;
+ 
+ };
 //function to retrieve the data and place it on the map
 function getData(map){
     //load the data
     $.ajax("data/emission.geojson", {
         dataType: "json",
         success: function(response){
-            //call function to create proportional symbols
-            createPropSymbols(response, map);
+            //create an attributes array
+            var attributes = processData(response);
+            createPropSymbols(response, map, attributes);
+            createSequenceControls(map, attributes);
+            SearchLayer(response, map,searchLayer);
         }
     });
 };
+// function for serach layer, taking input layer form the createPropSymbols function output
+function SearchLayer(response, map,searchLayer){
+    var searchControl = new  L.control.search({
+        layer: searchLayer ,
+        propertyName: 'Name',// Specify which property is searched into.
+        marker: false, //setting marker to false so that they do not display on the map.
+        initial: false,
+        zoom: 11, 
+        textPlaceholder: 'Search...'
+      });
+      searchControl.on('search:locationfound', function(e) {
+		
+		//console.log('search:locationfound', );
+		//map.removeLayer(this._markerSearch)
+		//e.layer.setStyle({fillColor: '#3f0', color: '#0f0'});
+		if(e.layer._popup)
+			e.layer.openPopup();
+	}).on('search:collapsed', function(e) {
+		featuresLayer.eachLayer(function(layer) {	//restore feature color
+			featuresLayer.resetStyle(layer);
+		});	
+	});
+      map.addControl( searchControl );
+};
 
+
+function createSequenceControls(map,attributes){
 $('#panel').append('<input class="range-slider" style="margin: 10px 10px 10px 10px;width: 93%;" type="range">');
-$('#panel').append('<button class="skip btn-sm btn btn-outline-danger" id="reverse">Reverse</button>');
-$('#panel').append('<button class="skip btn-sm btn btn-outline-warning" id="forward">Skip</button>');
+$('#panel').append('<button class="skip btn-sm btn btn-outline-danger" id="reverse"><i class="fas fa-angle-double-left"> Reverse</i></button>');
+$('#panel').append('<button class="skip btn-sm btn btn-outline-warning" id="forward">Skip <i class="fas fa-angle-double-right"></i></button>');
 
     //set slider attributes
     $('.range-slider').attr({
@@ -116,5 +172,55 @@ $('#panel').append('<button class="skip btn-sm btn btn-outline-warning" id="forw
         value: 0,
         step: 1
     });
+ //input listener for slider
+    $('.range-slider').on('input', function(){
+        //sequence
+        var index = $(this).val();
+        updatePropSymbols(map, attributes[index]);
+    });
+    $('.skip').click(function(){
+         //get the old index value
+         var index = $('.range-slider').val();
+         //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //if past the last attribute, wrap around to first attribute
+            index = index > 6 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            // if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 6 : index;
+        };
+
+        // update slider
+        $('.range-slider').val(index);
+        updatePropSymbols(map, attributes[index]);
+       
+    });
+
+   
+};
+function updatePropSymbols(map, attribute){
+    map.eachLayer(function(layers){
+        if (layers.feature && layers.feature.properties[attribute]){
+            //access feature properties
+            var props = layers.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layers.setRadius(radius);
+
+            //add formatted attribute to panel content string
+            var popupContent = "<p><b>State:</b> " + props.State + "</p><p><b>" + attribute + ":</b> " + props[attribute] + "</p><p>Values of Carbon are in Metric tons</p><p><b>Energy Source: </b>"+ props.Energy+ "</p><p><b>";
+
+            //replace the layer popup
+            layers.bindPopup(popupContent, {
+                offset: new L.Point(0,-radius)
+            });
+        };
+    });
+};
+
+
 
 $(document).ready(createMap); // calling create map function on document ready
